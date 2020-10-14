1c5a9e89f692fff3a6e8bde2ab7cde431822402b
Gustavo Silva <gustavo-silva14@hotmail.com>
2020-10-14 20:15:50 -0300

    teste commit

---
+++ GameTOP.lib/Jogador3.cs
@@ -0,0 +1,22 @@
+using GameTOP.Interface;
+
+namespace GameTOP.lib
+{
+    public class Jogador3 : iJogador
+    {
+        public string Chuta()
+        {
+            return "teste chuta";
+        }
+
+        public string Corre()
+        {
+            return "teste corre";
+        }
+
+        public string Passe()
+        {
+            return "teste corre";
+        }
+    }
+}
